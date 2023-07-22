9c5b9ae7b684f827dd9b9677f363326d985e17cd
eric-blake <>
2023-07-22 09:16:08 +0000

    Create print_board function

--- run.py
+++ run.py
@@ -8,4 +8,20 @@ def make_board():
     return board
 
 board = make_board()
-print(board)
+
+
+
+def print_board():
+    """Print out board grid using bars and dashed line"""
+    for column in range(BOARD_COLS):
+        print( f"   {column + 1}  ", end="") 
+    for row in range(BOARD_ROWS):
+        print(f" \n {'-' * (BOARD_COLS * 6)}")
+        print('|', end="")
+        for column in range(BOARD_COLS):
+            print(f" {board[row][column]}   |", end="")
+    print(f" \n {'-' * (BOARD_COLS * 6)}")
+    print("\n")
+    return board
+   
+print_board()
