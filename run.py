75e7565145de8d25e839b2f733931ade84b58aae
eric-blake <>
2023-07-22 09:08:30 +0000

    Create make_board function

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+BOARD_COLS = 7
+BOARD_ROWS = 6
+
+
+def make_board():
+    """Sets the board size"""
+    board = [[" " for column in range(BOARD_COLS)] for row in range(BOARD_ROWS)]
+    return board
+
+board = make_board()
+print(board)
